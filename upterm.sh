c497c1be16a27def8bb17ad405c21f71268b21c8
Dirk Arnez <smalldirkalex@gmail.com>
2023-11-02 04:05:53 +0800

    - update upterm.sh

--- upterm.sh
+++ upterm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -cO upterm https://ghproxy.com/https://github.com/corvofeng/upterm/releases/download/v0.9.0/upterm_linux_amd64
+wget -cO upterm https://github.com/corvofeng/upterm/releases/download/v0.9.0/upterm_linux_amd64
 chmod +x upterm
-./upterm host --vscode --verify-host-key=false $@ -- bash
+./upterm host --vscode --verify-host-key=false $@ -- upterm session current
